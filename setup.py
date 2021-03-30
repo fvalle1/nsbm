a851900fbe78c9f3fea7da735ce9aa67fb93064b
Filippo Valle <filippo.valle@to.infn.it>
2021-03-30 18:43:38 +0200

    change py version

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     install_requires = ["numpy", "pandas", "matplotlib", "cloudpickle"],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
 )
