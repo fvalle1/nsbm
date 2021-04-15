ba17974eba082b5493b29d885cd5b3f141a60b54
Filippo Valle <filippo.valle@to.infn.it>
2021-04-15 09:51:32 +0200

    verify indes are str

--- trisbm/trisbm.py
+++ trisbm/trisbm.py
@@ -74,7 +74,7 @@ class trisbm(sbmtm):
         df_all = df.copy(deep =True)
         for ikey,df_keyword in enumerate(df_keyword_list):
             df_keyword = df_keyword.reindex(columns=df.columns)
-            df_keyword.index = ["".join(["#" for _ in range(ikey+1)])+keyword for keyword in df_keyword.index]
+            df_keyword.index = ["".join(["#" for _ in range(ikey+1)])+str(keyword) for keyword in df_keyword.index]
             df_keyword["kind"] = ikey+2
             df_all = df_all.append(df_keyword)
 
