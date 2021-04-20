1281314691a912207ec9883f0a5c16977f4dcc0f
Filippo Valle <filippo.valle@to.infn.it>
2021-04-20 12:18:02 +0200

    add license

--- trisbm/sbmtm.py
+++ trisbm/sbmtm.py
@@ -1,3 +1,29 @@
+"""
+This module is cloned from https://github.com/martingerlach/hSBM_Topicmodel/commit/261d870cfc884c4f23ddaa213d07ccbddf348c78
+
+
+Copyright(C) 2020 martingerlach
+
+This program is free software: you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY
+without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see < http: // www.gnu.org/licenses/>.
+"""
+
+
+
+
+
+
 from __future__ import print_function
 import pandas as pd
 import numpy as np
