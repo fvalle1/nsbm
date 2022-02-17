070b79a7cc433f761760449c6fc8c55a8ab75cf0
Filippo Valle <fvalle@to.infn.it>
2022-02-17 11:24:07 +0100

    introduce consensus

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="trisbm",
-    version="0.4.2",
+    version="0.5.0",
     author="Filippo Valle",
     author_email="filippo.valle@unito.it",
     description="Package to run nSBM model.",
