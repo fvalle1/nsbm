c0d5c63dac207165c635f6916a227ab21681633c
Filippo Valle <filippo.valle@unito.it>
2022-05-19 09:01:32 +0200

    change theme

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,9 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.1',
+    VERSION: '0.5.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'singlehtml',
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: true,
